773ad24a1a5a6dc50b787cce0104ea539d010d5d
yarenbesler <yarenbesler2@gmail.com>
2025-04-29 19:53:42 +0300

    spor salonu web sitesi
    web sitesi yapıldı

---
+++ WEB TASARIM YAREN B ESLER-20232452007/index.php
@@ -0,0 +1,166 @@
+  <!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <div id="container">
+    <div id="header">
+        <div id="logo"> 
+          <a href="index.html"><img src="images/fitness-3.png" id="fotograf"></a>
+        </div> 
+        <div id="menü">
+          <nav class="navbar navbar-expand-lg navbar-light bg-#f8f9fa">
+            <a class="navbar-brand" href="#" id="deneme"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Anasayfa <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="hakkimizda.html">Hakkımızda</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#footer">İletişim</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Kampanyalar
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Bir Aylık 900₺</a>
+                    <a class="dropdown-item" href="#">Üç Aylık 2100₺</a>
+                    <a class="dropdown-item" href="#">Bir Yıllık 6000₺</a>
+                  </div>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#antrenör">Antrenörlerimiz</a>
+                  </li>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+        <div id="social"></div>
+    </div>
+      
+    <div>
+      <div class="main-image">
+        <img src="/images/kalite akıyo.jpg" alt="">
+      </div>
+
+      <div class="üç-kutu">
+        <div class="üç-kutu-içi kutu-bir">
+          <h1>Üyelik Paketlerimiz</h1>
+          <p>1 Aylık :900TL</p>
+          <p>3 Aylık :2100TL</p>
+          <p>6 Aylık :3600TL</p>
+          <p>12 Aylık :6000TL</p>
+          <p>Günlük giriş :150TL</p>
+          <p>1 Aylık kickbox :500TL</p>
+        </div>
+        <div class="üç-kutu-içi kutu-iki">
+          <h1>Hizmetlerimiz</h1>
+          <p>Birebir Antrenman</p>
+          <p>Kilo Verme-Kilo Alma</p>
+          <p>Kickbox</p>
+          <p>Fitness</p>
+          <p>Sıkılaşma</p>
+        </div>
+        <div class="üç-kutu-içi kutu-üç">
+          <h1>Hakkımızda</h1>
+          <p>Sportfit,İzmir'in Urla bölgesinde,spor ve fitness meraklılarına mükemmel bir deneyim sunan öncü bir spor salonudur.Uzun yıllara dayanan tecrübesi ve profesyonel eğitmen kadrosu ile bireylerin sağlık ve yaşam kailtelerini yükseltmeye odaklanan Sportfit,spor salonunda aranan tüm özellikleri bünyesinde barındırır.</p>
+        </div>
+      </div>
+
+      <div class="kilo-hakkında">
+        <div class="kilo-hakkında-kutu içi" data-aos="fade-down">
+          <h1>Kilo Verme-Kilo Alma</h1>
+          <p>Urla spor salonu olarak kişiye özel planlamış olduğumuz antrenman programı ile birlikte sizlerin hedeflemiş olduğu kilo verme-kilo alma yönünde çalışmalarımızı gerçekleştirmekteyiz.Sizlerde kilo vermek veya kilo almak istiyorsanız,İzmir'in Urla ilçesindeki spor merkezimzize katılım sağlayabilirsiniz.</p>
+        </div>
+        <div class="kilo-hakkında-kutu içi" data-aos="fade-down">
+          <h1>Nasıl Kilo Veririm?</h1>
+          <p>Urla spor salonu;gerek ekipman gerek antrenör desteği ile birlikte fazlalık olan kilolarınızdan kurtulabilirsiniz.Profesyonel antrenör ekibimiz ile birlikte fit bir vücuda sahip olabilirsiniz.Kilo vermeye yönelik yapmış olduğumuz çalışmalar doğrultusunda artık aynalar ile barışmanız mümkündür.</p>
+        </div>
+        <div class="kilo-hakkında-kutu içi" data-aos="fade-down">
+          <h1>Kas Kütlesi Artırma</h1>
+          <p>Birçok kişinin aklına gelen "kas kütlemi nasıl artırırım"sorusu vardır.Sportfit olarak hedeflemiş olduğumuz vücuda yönelik antrenmanlarımızı planlayarak sağlıklı bir şekilde kas kütlenizin artmasını sağlamaktayız.Uzman antrenör eşliğinde keyifli sporlar yapabilmemiz için tüm desteğimizi sağlamaktayız.</p>
+        </div>
+      </div>
+
+      <div class="boxes" id="antrenör">
+        <h1>ANTRENÖRLERİMİZ</h1>
+       <div class="boxes-upper">
+        <div class="boxes-inside">
+          <img src="images/yareeen.jpg" alt=""> 
+          <h3>Yaren Besler</h3>
+        </div>
+        <div class="boxes-inside">
+          <img src="images/berkeee.jpg" alt="">
+          <h3>Berke Kanber</h3>
+        </div>
+        <div class="boxes-inside">
+          <img src="images/ceyliin.jpg" alt="">
+          <h3>Ceylin Ün</h3>
+        </div>
+        <div class="boxes-inside">
+          <img src="images/ardaaaaaaa.jpg" alt="">
+          <h3>Arda Güngör</h3>
+        </div>
+       </div>
+      </div>
+    </div>
+    
+  <form    action="#" method="POST">
+    
+    <div id="uye-ol">
+      <div>
+        <h2>Üye ol!</h2>
+      </div>
+      <div class="input">
+        <input type="text" placeholder="Ad">
+        <input type="text" placeholder="Soyad">
+        <input placeholder="Mail Adresiniz" type="email" name="email">
+        <input placeholder="Şifre" name="message">
+        
+      </div>
+      
+      <button type="submit">Gönder</button>
+    </div>
+    
+  </form>
+
+
+
+    <div id="footer">
+        <div id="iletisim">
+          <p>Hacı İsa Mah 75.Yıl Cumhuriyet Sok no : 5, 35430 Urla/İzmir</p>
+        </div>
+        <div id="harita">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12520.258719721345!2d26.768389!3d38.324333!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14bb93619c1a5ef5%3A0x9ceefd0cf40c6e1c!2sSportfit!5e0!3m2!1str!2str!4v1702376535769!5m2!1str!2str" width="100%" height="221" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+
+    <i class="fa-brands fa-instagram"></i>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+  </body>
+</html>
